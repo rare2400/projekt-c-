55c4e010b60cbc6d628f1f1f96baa24acf97d73e
Ramona <rare2400@student.miun.com>
2026-01-02 09:40:42 +0100

    skapar en modell för strukturen på en utgift med setter och getter

---
+++ ExpenseTracker/models/Expense.cs
@@ -0,0 +1,18 @@
+/* 
+Programmering i C#.NET
+Projektuppgift av Ramona Reinholdz
+Konsolapp för att hantera och se sina utgifter
+
+Modell för utgift.
+ */
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public decimal Amount { get; set;}  //belopp
+        public string? Category { get; set;}    //kategori
+        public string? Description { get; set;} //beskrivning av utgift
+        public DateTime Date { get; set;}   //datum för utgift
+    }
+}
